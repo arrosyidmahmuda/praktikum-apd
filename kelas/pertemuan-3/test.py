434ec41678aa2c1556d0fa4caa7cefa9c71eeb45
Arrosyid Mahmuda <arrosyidmahmuda@gmail.com>
2025-09-28 22:55:41 +0800

    tugas

---
+++ kelas/pertemuan-3/test.py
@@ -0,0 +1,10 @@
+nilai = float(input("masukkan nilai ipk="))
+
+if nilai >= 3.2:
+    print("Asoy mendapatkan ipk yang memuaskan")
+    print("Asoy mentraktir kawannya kebab:")
+    
+    for i in range(1,5):
+        print(f"mentraktir kawannya kebab{i}")
+else:
+    print("Asoy mendapatkan ipk kurang memuaskan.")
