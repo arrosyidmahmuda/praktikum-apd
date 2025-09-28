7d040bc83a3ec08b51a361614fb624d7a71314cf
Arrosyid Mahmuda <arrosyidmahmuda@gmail.com>
2025-09-28 22:35:56 +0800

    tugas3

---
+++ kelas/pertemuan-2/test.py
@@ -0,0 +1,12 @@
+#codingan percabangan dan perulangan
+
+nilai = float(input("masukkan nilai ipk="))
+
+if nilai >= 3.2:
+    print("Asoy mendapatkan ipk yang memuaskan")
+    print("Asoy mentraktir kawannya kebab:")
+    
+    for i in range(1,5):
+        print(f"mentraktir kawannya kebab{i}")
+else:
+    print("Asoy mendapatkan ipk kurang memuaskan.")
